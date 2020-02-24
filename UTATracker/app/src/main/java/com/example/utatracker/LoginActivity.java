ce723843f7970ae0f2beae69573e6fe86fb7d70a
Joshua Morrell <morrelljosh614@gmail.com>
2020-02-24 13:34:44 -0700

    login stuff

---
+++ UTATracker/app/src/main/java/com/example/utatracker/LoginActivity.java
@@ -0,0 +1,92 @@
+package com.example.utatracker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText emailInput, passwordInput;
+    private Button loginBtn;
+    private ProgressBar progressBar;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_registration);
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.signOut();
+
+        if(mAuth.getCurrentUser() != null){
+            setContentView(R.layout.activity_user_registration);
+            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+            startActivity(intent);
+        }
+
+        initializeUI();
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser();
+            }
+        });
+        progressBar.setVisibility(View.INVISIBLE);
+    }
+
+    private void loginUser() {
+        progressBar.setVisibility(View.VISIBLE);
+        String email, password;
+        email = emailInput.getText().toString();
+        password = passwordInput.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Please enter email...", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Please enter password!", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Login successful!", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
+
+                            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(), "Login failed! Please try again.", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+    }
+
+
+    private void initializeUI() {
+        emailInput = findViewById(R.id.emailField);
+        passwordInput = findViewById(R.id.passwordField);
+        loginBtn = findViewById(R.id.loginBtn);
+        progressBar = findViewById(R.id.progressBar);
+    }
+}
