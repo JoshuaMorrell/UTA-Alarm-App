99555e477c4f99d54294e1dedcac3744cd6c087a
Kaelin Hoang <kaelin.hoang@gmail.com>
2020-02-24 15:54:33 -0700

    log out drop down menu functionality

--- UTATracker/app/src/main/java/com/example/utatracker/MainActivity.java
+++ UTATracker/app/src/main/java/com/example/utatracker/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSeleted(MenuItem item) {
+        int res_id = item.getItemId();
+        if(res_id == R.id.s)
     }
 
 }
