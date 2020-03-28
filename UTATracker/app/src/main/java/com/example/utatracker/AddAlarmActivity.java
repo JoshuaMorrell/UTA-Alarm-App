cccea219d9f0fb3ce6eeaf644f0d819a92cd1d2d
lynguyen <khanhly.t.nguyen@gmail.com>
2020-03-27 18:02:49 -0600

    add minor functionality when creating alarm.

--- UTATracker/app/src/main/java/com/example/utatracker/AddAlarmActivity.java
+++ UTATracker/app/src/main/java/com/example/utatracker/AddAlarmActivity.java
@@ -6,18 +6,24 @@ import androidx.transition.AutoTransition;
 import androidx.transition.TransitionManager;
 
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.PopupMenu;
 import android.widget.RelativeLayout;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-public class AddAlarmActivity extends AppCompatActivity {
+public class AddAlarmActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
+    Button saveButton;
 
     ConstraintLayout dateExpandable, timeExpandable, notifyExpandable;
     RelativeLayout dateLayout, timeLayout, notifyLayout;
@@ -30,6 +36,15 @@ public class AddAlarmActivity extends AppCompatActivity {
 
         dateExpandable = findViewById(R.id.dateExpandView);
         dateLayout = findViewById(R.id.date);
+        saveButton = findViewById(R.id.saveButton);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View v) {
+                  startActivity(new Intent(AddAlarmActivity.this, HomeActivity.class));
+              }
+        });
+
         dateLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -58,5 +73,29 @@ public class AddAlarmActivity extends AppCompatActivity {
                 timePicker.show();
             }
         });
+    }
+
+    public void selectStartLocation(View v) {
+        PopupMenu popupMenu = new PopupMenu(this, v);
+        popupMenu.setOnMenuItemClickListener(this);
+        popupMenu.inflate(R.menu.popup_menu);
+        popupMenu.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.firstStop:
+                Toast.makeText(this, "FIRST STOP", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.secondStop:
+                Toast.makeText(this, "SECOND STOP", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.thirdStop:
+                Toast.makeText(this, "THIRD STOP", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return false;
+        }
     }
 }
